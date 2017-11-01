5b533963359160e1ea39ef21a3893ab482e09121
Clayton Wilson <claytonwwilson97@gmail.com>
2017-11-01 16:25:56 -0400

    Added skeleton menu for launcher.py

--- launcher.py
+++ launcher.py
@@ -1,8 +1,43 @@
-# this will be the launcher for the project
-print("1. Search theTVDB.com")
-print("2. Clear download folders")
-print("3. Change login")
-print("4. Refresh API Token")
-print("5. Install Requirements")
-print("6. Check for updates")
-print("7. Exit")
+import os
+def clear_screen():
+    IS_WINDOWS = os.name == "nt"
+    if IS_WINDOWS:
+        os.system("cls")
+    else:
+        os.system("clear")
+
+def user_choice():
+    return input("> ").lower().strip()
+
+
+while True:
+    print("1. Search theTVDB.com")
+    print("2. Clear download folders")
+    print("3. Change login")
+    print("4. Refresh API Token")
+    print("5. Install Requirements")
+    print("6. Check for updates")
+    print("7. Exit")
+
+    choice = user_choice()
+
+    if choice == "1":
+        print("Search")
+        break
+    elif choice == "2":
+        print("Clear download")
+        break
+    elif choice == "3":
+        print("Change login")
+        break
+    elif choice == "4":
+        print("Refresh token")
+        break
+    elif choice == "5":
+        print("install requirements")
+        break
+    elif choice == "6":
+        print("update")
+        break
+    elif choice == "7":
+        print("Exit")
