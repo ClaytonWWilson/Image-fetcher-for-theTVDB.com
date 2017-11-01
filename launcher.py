147a2220cd9544dd3376c02d5b2e6e139bd3f9be
Clayton Wilson <claytonwwilson97@gmail.com>
2017-11-01 13:36:31 -0400

    Added the launcher for the project

---
+++ launcher.py
@@ -0,0 +1,8 @@
+# this will be the launcher for the project
+print("1. Search theTVDB.com")
+print("2. Clear download folders")
+print("3. Change login")
+print("4. Refresh API Token")
+print("5. Install Requirements")
+print("6. Check for updates")
+print("7. Exit")
